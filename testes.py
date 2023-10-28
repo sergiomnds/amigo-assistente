750f10b27536c31582bfba0bb8080a401bc70009
Sergio Mendes <srgmendesl@gmail.com>
2023-10-27 23:28:21 -0300

    teste com o nome

--- testes.py
+++ testes.py
@@ -11,6 +11,31 @@ PREVISAO_AMANHA = "audios/previsao_amanha.wav"
 CONDICAO_CLIMATICA = "audios/condicao_climatica.wav"
 PREVISAO_CHUVA = "audios/previsao_chuva.wav"
 
+class TesteNomeDoAssistente(unittest.TestCase):
+    def testar_01_reconhecer_nome(self):
+        palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
+        
+        r = sr.Recognizer()
+        with sr.AudioFile(CHAMANDO_AMIGO) as source:
+            audio = r.listen(source)
+            transcricao = r.recognize_google(audio, language="pt-BR")
+
+        tokens = tokenizar(transcricao.lower())
+        tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
+        resultado = self.assertEqual(tokens[0], "amigo")
+
+    def testar_02_nao_reconhecer_nome(self):
+        palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
+        
+        r = sr.Recognizer()
+        with sr.AudioFile(CHAMANDO_OUTRO_NOME) as source:
+            audio = r.listen(source)
+            transcricao = r.recognize_google(audio, language="pt-BR")
+
+        tokens = tokenizar(transcricao.lower())
+        tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
+        self.assertNotEqual(tokens[0], "amigo")
+
 class TestAtuadores(unittest.TestCase):
         
     def test_atuar_sobre_atual(self):
@@ -25,9 +50,6 @@ class TestAtuadores(unittest.TestCase):
         tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
         resultado = atuar_sobre_atual(tokens[1], tokens[2])
         self.assertTrue(isinstance(resultado, bool))
-        
-        if resultado:
-            print("O teste de atuar_sobre_atual foi bem-sucedido!")
 
     def test_atuar_sobre_tempo(self):
         palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
@@ -41,9 +63,6 @@ class TestAtuadores(unittest.TestCase):
         tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
         resultado = atuar_sobre_tempo(tokens[1], tokens[2])
         self.assertTrue(isinstance(resultado, bool))
-        
-        if resultado:
-            print("O teste de atuar_sobre_tempo foi bem-sucedido!")
 
     def test_atuar_sobre_climatica(self):
         palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
@@ -57,9 +76,6 @@ class TestAtuadores(unittest.TestCase):
         tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
         resultado = atuar_sobre_climatica(tokens[1], tokens[2])
         self.assertTrue(isinstance(resultado, bool))
-        
-        if resultado:
-            print("O teste de atuar_sobre_climatica foi bem-sucedido!")
 
     def test_atuar_sobre_chover(self):
         palavras_de_parada = set(corpus.stopwords.words(IDIOMA_CORPUS))
@@ -73,9 +89,6 @@ class TestAtuadores(unittest.TestCase):
         tokens = eliminar_palavras_de_parada(tokens, palavras_de_parada)
         resultado = atuar_sobre_chover(tokens[1], tokens[2])
         self.assertTrue(isinstance(resultado, bool))
-        
-        if resultado:
-            print("O teste de atuar_sobre_chover foi bem-sucedido!")
 
 if __name__ == '__main__':
     unittest.main()
